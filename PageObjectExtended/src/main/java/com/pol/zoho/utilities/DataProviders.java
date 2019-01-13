093b14769bff47cecfabd5436de3509b30bbcae5
Laxminarayan Jena <lnarayan.jena@gmail.com>
2019-01-13 10:44:30 +0530

    Page Object Extended

---
+++ PageObjectExtended/src/main/java/com/pol/zoho/utilities/DataProviders.java
@@ -0,0 +1,21 @@
+package com.pol.zoho.utilities;
+
+import java.lang.reflect.Method;
+
+import org.testng.annotations.DataProvider;
+
+public class DataProviders {
+	
+	@DataProvider(name="masterDP" ,parallel=true)
+	public static Object[][] getDataSuite1(Method m) {
+		System.out.println(m.getName());
+		ExcelReader excel = new ExcelReader(Constants.SUITE1_XL_PATH);
+		String testCase=m.getName();
+		return DataUtil.getData( testCase, excel);
+
+		
+	}
+
+	
+	
+}
--- PageObjectExtended/src/test/java/com/pol/zoho/testcases/LoginTest.java
+++ PageObjectExtended/src/test/java/com/pol/zoho/testcases/LoginTest.java
@@ -7,22 +7,27 @@ import org.testng.annotations.Test;
 import com.pol.zoho.PageObjects.ZohoHomePage;
 import com.pol.zoho.PageObjects.ZohoLoginPage;
 import com.pol.zoho.SetUp.BaseTest;
+import com.pol.zoho.utilities.Constants;
 import com.pol.zoho.utilities.DataProviders;
+import com.pol.zoho.utilities.DataUtil;
+import com.pol.zoho.utilities.ExcelReader;
 
-public class TestCase2 extends BaseTest  {
+public class LoginTest extends BaseTest {
+	
 	
 	
 	@Test(dataProviderClass=DataProviders.class,dataProvider="masterDP")
-	public void LoginTest(Hashtable<String,String> data ) throws InterruptedException
+	public void loginTest(Hashtable<String,String> data ) throws InterruptedException
 	{
 		
-		openBrowser(data.get("browser"));
 		
+		ExcelReader excel = new ExcelReader(Constants.SUITE1_XL_PATH);
+		DataUtil.checkExecution("master", "LoginTest", data.get("Runmode"), excel);
+		openBrowser(data.get("browser"));
 		ZohoHomePage home = new ZohoHomePage(getDriver());
 		ZohoLoginPage login =home.gotoLogin();		
 		login.doLogin(data.get("username"), data.get("password"));
 		closeBrowser();
-		
 		
 		
 	}
